83881e9d5ded0b74a6d3c45536dece46b64c43ba
dannyb94 <danielle.ussery@bryanuniversity.edu>
2021-06-27 23:54:44 -0700

    week3

--- week3/todo3/src/App.js
+++ week3/todo3/src/App.js
@@ -1,46 +1,27 @@
 import './App.css';
 import ToDo from './ToDoList';
 import { items } from './store';
+import React from "react";
 //import { useState } from 'react';
 //import { v4 as uuidv4 } from 'uuid';
 
-function App() {
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+    this.dltBtn = this.dltBtn.bind(this);
+  }
 
-  // const [dltBtn, setDltBtn] = useState('Delete');
-  // const byeBye = () => {
-  //   setDltBtn('X')
-  // }
-
-  // dltListed = (e) => {
-  //   let deleted = [...this.state.aryTodos];
-  //   let index = deleted.findIndex(item => item.id === e);
-  //   deleted.splice(index, 1)
-  //   this.setState({
-  //     aryTodos: deleted
-  //   })
-  // }
-
-  // render(){
-  //   const todoCards = this.state.aryTodos.map((item, index) => {
-  //     return (
-  //       <ToDo
-  //         key = {item.key}
-  //         id = {item.id}
-  //         text = {item.text}
-  //         isCompleted = {item.isCompleted}
-  //         deleteListed = {this.deleteListed}
-  //       />
-  //     )
-  //   })
-  // }
-  const dltBtn = <button onCLick = {dltListed}>X</button>
-
-  return (
-    <div>
-      <h1>Checklist</h1>
-      <ToDo items = {items}/>
-    </div>
-  )
+  dltBtn(){}
+  render(){
+    return (
+      <div>
+        <h1>Checklist</h1>
+        <ToDo items = {items}/>
+      </div>
+    )
+  }
 }
 
 export default App;
--- week3/todo3/src/ToDoList.js
+++ week3/todo3/src/ToDoList.js
@@ -1,29 +1,27 @@
 import React from "react";
+import { items } from './store';
 
-export default function ToDo({items}){
+export default class ToDo extends React.Component {
 
-    const listItems = items.map((item) => {
-        // completed(){
-        //     this.props.completed(this.props.completed(this.props.id))
-        // }
-
-        // render(){}
-
+    render(){
+        const listItems = items.map((item) => {
+            return (
+                <div key = {item.id}>
+                    <ul>
+                        <li>
+                            <input type="checkbox" class="items" checked={this.props.isCompleted} onChange={this.props.toggleComplete}></input>
+                            <label>{item.text}</label>
+                            <button onClick = {() => this.props.dltBtn(this.props.id)}>X</button>
+                        </li>
+                    </ul>
+                </div>
+            )
+        })
+    
         return (
-            <div key = {item.id}>
-                <ul>
-                    <li>
-                        <input type="checkbox" class="items" name="laundry" onclick={() => {this.completed()}}></input>
-                        <label>{item.text}</label>
-                    </li>
-                </ul>
-            </div>
+            <>
+                {listItems}
+            </>
         )
-    });
-
-    return (
-        <>
-            {listItems}
-        </>
-    )
+    };
 }
